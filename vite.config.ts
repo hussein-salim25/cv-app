c18f5330ef1332cd9c4d8a9aedd3c5812d7ed018
Hussein Salim <husseinsalim23@outlook.com>
2025-08-16 20:19:35 +0300

    Fix: vite config and dependencies for GitHub Pages deploy

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/cv-app/',  // 👈 tells Vite where your app will be served
+})
